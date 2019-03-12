e1bcd14f45c5c4aef4b0921c45e21049a1837982
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2019-03-12 00:12:52 +0000

    Render items correctly

--- src/Items/Items.test.js
+++ src/Items/Items.test.js
@@ -5,12 +5,51 @@ import { createRenderer } from '../test-utils';
 
 describe('Items', () => {
   let render;
+  const initProps = {
+    items: [
+      {
+        id: 1001,
+        name: 'Kale Caesar Pasta, Turmeric Satay Broccoli & Lemon Cashew Greens',
+        dietaries: ['v', 've', 'df', 'gf', 'n!'],
+      },
+    ],
+  };
 
   beforeAll(() => {
-    render = createRenderer(Items);
+    render = createRenderer(Items, initProps);
   });
 
   it('should render correctly', () => {
     expect(render()).toMatchSnapshot();
+  });
+
+  it('should render list of items from data prop', () => {
+    const mockItems = [
+      {
+        id: 1002,
+        name: 'Hake & Smoky Chickpeas, Brown Rice & Quinoa, Roasted Roots',
+        dietaries: ['gf', 'df', 'rsf'],
+      },
+      {
+        id: 1003,
+        name: 'Dill & Swiss Chard Potato Cakes, Summer Tabbouleh & Roasted Roots',
+        dietaries: ['gf', 'df', 'v', 've', 'n!'],
+      },
+      {
+        id: 1004,
+        name: 'Hake & Smoky Chickpeas, Herby Potatoes & Turmeric Satay Broccoli',
+        dietaries: ['df', 'gf', 'rsf', 'n!'],
+      },
+      {
+        id: 1005,
+        name: 'Cuban Beef, Brown Rice & Quinoa, Green Pepper & Butterbean Salad',
+        dietaries: ['gf', 'df'],
+      },
+    ];
+    const component = render({
+      items: mockItems,
+    });
+
+    expect(component.find('Item')).toHaveLength(mockItems.length);
   });
 });
--- src/Items/index.js
+++ src/Items/index.js
@@ -1,68 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Items = () => {
+import Item from '../Item';
+
+const Items = (props) => {
   return (
     <div className="col-4">
       <ul className="item-picker">
-      <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-        </li>
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-        </li>
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-        </li>
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-        </li>
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-        </li>
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-        </li>
-        <li className="item">
-          <h2>Dummy item</h2>
-          <p>
-            <span className="dietary">ve</span>
-            <span className="dietary">v</span>
-            <span className="dietary">n!</span>
-          </p>
-        </li>
+        {props.items.map(item => <Item key={item.id} />)}
       </ul>
     </div>
   );
+};
+
+Items.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      dietaries: PropTypes.arrayOf(PropTypes.string).isRequired,
+    })
+  ),
+};
+
+Items.defaultProps = {
+  items: [],
 };
 
 export default Items;
