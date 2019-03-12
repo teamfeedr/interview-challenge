ba9a80a1c7f90ed1ab4c8eb660f38a3b33502feb
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2019-03-12 01:34:09 +0000

    Render correct counts in Summary header

--- src/SummaryHeader/SummaryHeader.test.js
+++ src/SummaryHeader/SummaryHeader.test.js
@@ -5,9 +5,35 @@ import { createRenderer } from '../test-utils';
 
 describe('SummaryHeader', () => {
   let render;
+  const mockItems = [
+    {
+      id: 1002,
+      name: 'Hake & Smoky Chickpeas, Brown Rice & Quinoa, Roasted Roots',
+      dietaries: ['v', 've', 'rsf'],
+    },
+    {
+      id: 1003,
+      name: 'Dill & Swiss Chard Potato Cakes, Summer Tabbouleh & Roasted Roots',
+      dietaries: ['v', 've'],
+    },
+    {
+      id: 1004,
+      name: 'Hake & Smoky Chickpeas, Herby Potatoes & Turmeric Satay Broccoli',
+      dietaries: ['rsf'],
+    },
+    {
+      id: 1005,
+      name: 'Cuban Beef, Brown Rice & Quinoa, Green Pepper & Butterbean Salad',
+      dietaries: ['v', 'rsf'],
+    },
+  ];
+  const initProps = {
+    totalCount: mockItems.length,
+    items: mockItems,
+  };
 
   beforeAll(() => {
-    render = createRenderer(SummaryHeader);
+    render = createRenderer(SummaryHeader, initProps);
   });
 
   it('should render correctly', () => {
--- src/SummaryHeader/index.js
+++ src/SummaryHeader/index.js
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-const SummaryHeader = () => (
-  <div className="menu-summary">
-    <div className="container">
-      <div className="row">
-        <div className="col-6 menu-summary-left">
-          <span>5 items</span>
-        </div>
-        <div className="col-6 menu-summary-right">
-          6x <span className="dietary">ve</span>
-          4x <span className="dietary">v</span>
-          12x <span className="dietary">n!</span>
+import Item from '../Item';
+
+class SummaryHeader extends Component {
+  getDietaryCounts() {
+    const dietaries = {};
+
+    this.props.items.forEach(item => {
+      item.dietaries.forEach(dietaryItem => {
+        if (dietaries[dietaryItem] === undefined) {
+          dietaries[dietaryItem] = 0;
+        } else {
+          dietaries[dietaryItem] += 1;
+        }
+      })
+    });
+
+    return dietaries;
+  }
+
+  renderDietaryCounts() {
+    const counts = this.getDietaryCounts();
+
+    return Object.keys(counts).map(dietaryName => {
+      const dietaryCount = counts[dietaryName];
+
+      return (
+        <Fragment key={dietaryName}>
+          {dietaryCount}x <span className="dietary">{dietaryName}</span>
+        </Fragment>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className="menu-summary">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 menu-summary-left">
+              <span>{this.props.items.length} items</span>
+            </div>
+
+            <div className="col-6 menu-summary-right">
+              {this.renderDietaryCounts()}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    )
+  }
+}
+
+SummaryHeader.defaultProps = {
+  items: [],
+}
+
+SummaryHeader.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape(Item.propTypes)),
+}
 
 export default SummaryHeader;
