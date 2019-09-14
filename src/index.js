57dd0c5a872c212532cc356bcae7018bf005aa82
Andreas Goodstein <andreasgoodstein@gmail.com>
2019-09-14 19:40:02 +0200

    update App initialization

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
-import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+
+import items from "./items";
+
+render(<App items={items} />, document.getElementById("root"));
 registerServiceWorker();
